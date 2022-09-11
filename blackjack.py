1507740e8c470069e6fe58d103d466cb18e40f86
Rafael Bechara <75490904+raeef96@users.noreply.github.com>
2022-09-11 12:20:46 +0200

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,10 @@
-import random 
+import random
+from secrets import choice 
 
 
 # first we need to create a deck of cards 
 
-deck = [1,2,3,4,5,6,7,8,9,'A','J','Q','K']
+deck = [2,3,4,5,6,7,8,9,'A','J','Q','K']
 def multideck():
     for i in range (2):
         deck.extend(deck)
@@ -19,6 +20,17 @@ print(deck)
 
 #  create a hand for the player and the dealer
 
+# deal cards
+def deal(turn):
+    drawnCard = random.choice(deck)
+    turn.append(drawnCard)
+    deck.remove(drawnCard)
+
 # calculate the total of the hands
+def handTotal(turn):
+    total = 0
+    for card in turn:
+        if card in range (1,11):
+            total += card
 
 # use game logic to check for winner
